1b4a58fcd634aaf41a1b596d3bc70e17fd3b5e08
Gabriela Pellin Perosa <gpp260203@gmail.com>
2022-12-27 15:45:02 +0000

    Exercicio 4

---
+++ ex004.py
@@ -0,0 +1,7 @@
+#Desafio 03
+#Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média
+print("Digite suas notas para saber a média")
+n1 = float(input("Primeira nota: "))
+n2 = float(input("Segunda nota: "))
+m = (n1+n2)/2
+print("Sua média é {}".format(m))
